2bbd2f4337293dc376d309eedf369e4e6742965f
Soner Demirci <116302869+Soner08@users.noreply.github.com>
2022-11-28 21:22:25 +0300

    İsci
    www.patika.dev

---
+++ Isci/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Isci employee = new Isci ("Soner DEMİRCİ",10000,45 , 2003);
+
+
+        String s = employee.toString();
+        System.out.println(s);
+    }
+}
